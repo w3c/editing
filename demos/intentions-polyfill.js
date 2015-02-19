5e50df0fa46c09c5b16903ac00eb0a5f1b187407
Ben Peters <benjamp@microsoft.com>
2015-02-19 15:17:53 -0800

    Updated Demo page for CE=Typing

--- demos/intentions-polyfill.js
+++ demos/intentions-polyfill.js
@@ -4,10 +4,9 @@
 // This file works in IE9+, Chrome, and Firefox
 
 (function(){
-    // [[Intention, modifier_key, keycode, event], ...]
+    // [[inputType, modifier_key, keycode, event, data], ...]
     var supportedIntentions = [];
     populateSupportedIntentions();
-    var handleDefaultsInScript = true;
     
     (function () {
         function CustomEvent ( event, params ) {
@@ -24,37 +23,29 @@
     
     function fireIntentionEvent (intentionDetails){
         var intentionEvent;
-        var intention = intentionDetails[0];
+        var inputType = intentionDetails[0];
+        var data = intentionDetails[4];
         var eventType = intentionDetails[3];
-        switch (intention)
+        switch (inputType)
         {
         default:
-            intentionEvent = new CustomEvent(eventType, {"cancelable":true, "detail":{"intention":intention}});
+            intentionEvent = new CustomEvent(eventType, {"cancelable":true, "detail":{"inputType":inputType, "data": data}});
         }
         return document.activeElement.dispatchEvent(intentionEvent);
-    };
+    }
     
-    function handleIntention (intentionDetails){
-        var intention = intentionDetails[0];
-        var retVal = false;
-        switch (intention) {
-        default:
+    function performDefaultBehavior(intentionDetails, triggeringEvent) {
+        var inputType = intentionDetails[0];
+        switch (inputType) {
+        case "replaceText":
+            var targetStartContainer = document.getSelection().getRangeAt(0).startContainer;
+            if (targetStartContainer != document.getSelection().getRangeAt(0).endContainer) {
+                triggeringEvent.preventDefault(); // The selection crosses nodes, so typing has no default behavior
+            }
             break;
+        default:
+            triggeringEvent.preventDefault(); //All other inputTypes have no default behavior, so prevent the browser's behavior
         }
-        return retVal;
-    };
-    
-    if (Object.defineProperty && CustomEvent && CustomEvent.prototype) {
-        Object.defineProperty(CustomEvent.prototype, "intention",    
-            { enumerable: true, configurable: true, 
-                get: function() {
-                    var result = null; 
-                    if (this.detail.intention) { 
-                        result = this.detail.intention; 
-                    } 
-                    return result; 
-                } 
-            });
     }
     
     //Post Page-Load Functionality
@@ -75,8 +66,8 @@
                                 if (cancelled)
                                 {
                                     evt.preventDefault(); // Prevent browser default behavior for this Action event
-                                } else if(handleDefaultsInScript && handleIntention(supportedIntentions[i])) {
-                                    evt.preventDefault(); // Prevent browser default if this was handled by script above
+                                } else {
+                                    performDefaultBehavior(supportedIntentions[i], evt);
                                 }
                                 break;
                             }
@@ -93,8 +84,8 @@
                                 if (cancelled)
                                 {
                                     evt.preventDefault(); // Prevent browser default behavior for this Action event
-                                } else if(handleDefaultsInScript && handleIntention(supportedIntentions[i])) {
-                                    evt.preventDefault(); // Prevent browser default if this was handled by script above
+                                } else {
+                                    performDefaultBehavior(supportedIntentions[i]);
                                 }
                                 break;
                             }
@@ -105,25 +96,6 @@
                 if (ceAttribute.indexOf('typing') >= 0) 
                 {
                     ceElement.setAttribute('contenteditable', 'true');
-                    ceElement.addEventListener("beforeInput", 
-                        function (evt) 
-                        {
-                            if (evt.intention != "insertText")
-                            {
-                                evt.preventDefault();
-                            }
-                        });
-                }
-                else if (ceAttribute.indexOf('selection') >= 0)
-                {
-                    ceElement.setAttribute('contenteditable', 'true');
-                    ceElement.addEventListener("beforeInput", function (evt) {evt.preventDefault()});
-                }
-                else if (ceAttribute.indexOf('cursor') >= 0)
-                {
-                    ceElement.setAttribute('contenteditable', 'true');
-                    ceElement.addEventListener("beforeSelectionChange", function (evt) {evt.preventDefault()});
-                    ceElement.addEventListener("beforeInput", function (evt) {evt.preventDefault()});
                 }
         }
     );
@@ -131,110 +103,102 @@
     function populateSupportedIntentions()
     {
         supportedIntentions = 
-           [["delete", "none", 8, "beforeInput"],
-            ["newline", "none", 13, "beforeInput"],
-            ["moveCaret", "none", 37, "beforeSelectionChange"],
-            ["moveCaret", "none", 38, "beforeSelectionChange"],
-            ["moveCaret", "none", 39, "beforeSelectionChange"],
-            ["moveCaret", "none", 40, "beforeSelectionChange"],
-            ["delete", "none", 46, "beforeInput"],
-            ["insertText", "none", 48, "beforeInput"],
-            ["insertText", "none", 49, "beforeInput"],
-            ["insertText", "none", 50, "beforeInput"],
-            ["insertText", "none", 51, "beforeInput"],
-            ["insertText", "none", 52, "beforeInput"],
-            ["insertText", "none", 53, "beforeInput"],
-            ["insertText", "none", 54, "beforeInput"],
-            ["insertText", "none", 55, "beforeInput"],
-            ["insertText", "none", 56, "beforeInput"],
-            ["insertText", "none", 57, "beforeInput"],
-            ["insertText", "none", 65, "beforeInput"],
-            ["insertText", "none", 66, "beforeInput"],
-            ["insertText", "none", 67, "beforeInput"],
-            ["insertText", "none", 68, "beforeInput"],
-            ["insertText", "none", 69, "beforeInput"],
-            ["insertText", "none", 70, "beforeInput"],
-            ["insertText", "none", 71, "beforeInput"],
-            ["insertText", "none", 72, "beforeInput"],
-            ["insertText", "none", 73, "beforeInput"],
-            ["insertText", "none", 74, "beforeInput"],
-            ["insertText", "none", 75, "beforeInput"],
-            ["insertText", "none", 76, "beforeInput"],
-            ["insertText", "none", 77, "beforeInput"],
-            ["insertText", "none", 78, "beforeInput"],
-            ["insertText", "none", 79, "beforeInput"],
-            ["insertText", "none", 80, "beforeInput"],
-            ["insertText", "none", 81, "beforeInput"],
-            ["insertText", "none", 82, "beforeInput"],
-            ["insertText", "none", 83, "beforeInput"],
-            ["insertText", "none", 84, "beforeInput"],
-            ["insertText", "none", 85, "beforeInput"],
-            ["insertText", "none", 86, "beforeInput"],
-            ["insertText", "none", 87, "beforeInput"],
-            ["insertText", "none", 88, "beforeInput"],
-            ["insertText", "none", 89, "beforeInput"],
-            ["insertText", "none", 90, "beforeInput"],
-            ["insertText", "none", 96, "beforeInput"],
-            ["insertText", "none", 97, "beforeInput"],
-            ["insertText", "none", 98, "beforeInput"],
-            ["insertText", "none", 99, "beforeInput"],
-            ["insertText", "none", 100, "beforeInput"],
-            ["insertText", "none", 101, "beforeInput"],
-            ["insertText", "none", 102, "beforeInput"],
-            ["insertText", "none", 103, "beforeInput"],
-            ["insertText", "none", 104, "beforeInput"],
-            ["insertText", "none", 105, "beforeInput"],
-            ["insertText", "none", 107, "beforeInput"],
-            ["insertText", "none", 109, "beforeInput"],
-            ["insertText", "none", 110, "beforeInput"],
-            ["insertText", "none", 111, "beforeInput"],
-            ["insertText", "none", 186, "beforeInput"],
-            ["insertText", "none", 187, "beforeInput"],
-            ["insertText", "none", 188, "beforeInput"],
-            ["insertText", "none", 189, "beforeInput"],
-            ["insertText", "none", 190, "beforeInput"],
-            ["insertText", "none", 219, "beforeInput"],
-            ["insertText", "none", 220, "beforeInput"],
-            ["insertText", "none", 221, "beforeInput"],
-            ["insertText", "none", 222, "beforeInput"],
-            ["modify", "shift", 37, "beforeSelectionChange"],
-            ["modify", "shift", 38, "beforeSelectionChange"],
-            ["modify", "shift", 39, "beforeSelectionChange"],
-            ["modify", "shift", 40, "beforeSelectionChange"],
-            ["insertText", "shift", 65, "beforeInput"],
-            ["insertText", "shift", 66, "beforeInput"],
-            ["insertText", "shift", 67, "beforeInput"],
-            ["insertText", "shift", 68, "beforeInput"],
-            ["insertText", "shift", 69, "beforeInput"],
-            ["insertText", "shift", 70, "beforeInput"],
-            ["insertText", "shift", 71, "beforeInput"],
-            ["insertText", "shift", 72, "beforeInput"],
-            ["insertText", "shift", 73, "beforeInput"],
-            ["insertText", "shift", 74, "beforeInput"],
-            ["insertText", "shift", 75, "beforeInput"],
-            ["insertText", "shift", 76, "beforeInput"],
-            ["insertText", "shift", 77, "beforeInput"],
-            ["insertText", "shift", 78, "beforeInput"],
-            ["insertText", "shift", 79, "beforeInput"],
-            ["insertText", "shift", 80, "beforeInput"],
-            ["insertText", "shift", 81, "beforeInput"],
-            ["insertText", "shift", 82, "beforeInput"],
-            ["insertText", "shift", 83, "beforeInput"],
-            ["insertText", "shift", 84, "beforeInput"],
-            ["insertText", "shift", 85, "beforeInput"],
-            ["insertText", "shift", 86, "beforeInput"],
-            ["insertText", "shift", 87, "beforeInput"],
-            ["insertText", "shift", 88, "beforeInput"],
-            ["insertText", "shift", 89, "beforeInput"],
-            ["selectall", "control", 65, "beforeSelectionChange"], 
-            ["format", "control", 66, "beforeInput"], 
-            ["copy", "control", 67, "beforeInput"], 
-            ["format", "control", 73, "beforeInput"], 
-            ["format", "control", 85, "beforeInput"], 
-            ["paste", "control", 86, "beforeInput"], 
-            ["cut", "control", 88, "beforeInput"],
-            ["redo", "control", 89, "beforeInput"], 
-            ["undo", "control", 90, "beforeInput"]];    
+           [["deleteContent", "none", 8, "beforeInput", "NULL"],
+            ["insertNewline", "none", 13, "beforeInput", "NULL"],
+            ["moveCaret", "none", 37, "beforeSelectionChange", "left"],
+            ["moveCaret", "none", 38, "beforeSelectionChange", "up"],
+            ["moveCaret", "none", 39, "beforeSelectionChange", "right"],
+            ["moveCaret", "none", 40, "beforeSelectionChange", "down"],
+            ["deleteContent", "none", 46, "beforeInput", "NULL"],
+            ["replaceText", "none", 48, "beforeInput", "0"],
+            ["replaceText", "none", 49, "beforeInput", "1"],
+            ["replaceText", "none", 50, "beforeInput", "2"],
+            ["replaceText", "none", 51, "beforeInput", "3"],
+            ["replaceText", "none", 52, "beforeInput", "4"],
+            ["replaceText", "none", 53, "beforeInput", "5"],
+            ["replaceText", "none", 54, "beforeInput", "6"],
+            ["replaceText", "none", 55, "beforeInput", "7"],
+            ["replaceText", "none", 56, "beforeInput", "8"],
+            ["replaceText", "none", 57, "beforeInput", "9"],
+            ["replaceText", "none", 65, "beforeInput", "a"],
+            ["replaceText", "none", 66, "beforeInput", "b"],
+            ["replaceText", "none", 67, "beforeInput", "c"],
+            ["replaceText", "none", 68, "beforeInput", "d"],
+            ["replaceText", "none", 69, "beforeInput", "e"],
+            ["replaceText", "none", 70, "beforeInput", "f"],
+            ["replaceText", "none", 71, "beforeInput", "g"],
+            ["replaceText", "none", 72, "beforeInput", "h"],
+            ["replaceText", "none", 73, "beforeInput", "i"],
+            ["replaceText", "none", 74, "beforeInput", "j"],
+            ["replaceText", "none", 75, "beforeInput", "k"],
+            ["replaceText", "none", 76, "beforeInput", "l"],
+            ["replaceText", "none", 77, "beforeInput", "m"],
+            ["replaceText", "none", 78, "beforeInput", "n"],
+            ["replaceText", "none", 79, "beforeInput", "o"],
+            ["replaceText", "none", 80, "beforeInput", "p"],
+            ["replaceText", "none", 81, "beforeInput", "q"],
+            ["replaceText", "none", 82, "beforeInput", "r"],
+            ["replaceText", "none", 83, "beforeInput", "s"],
+            ["replaceText", "none", 84, "beforeInput", "t"],
+            ["replaceText", "none", 85, "beforeInput", "u"],
+            ["replaceText", "none", 86, "beforeInput", "v"],
+            ["replaceText", "none", 87, "beforeInput", "w"],
+            ["replaceText", "none", 88, "beforeInput", "x"],
+            ["replaceText", "none", 89, "beforeInput", "y"],
+            ["replaceText", "none", 90, "beforeInput", "z"],
+            ["replaceText", "none", 106, "beforeInput", "multiply"],
+            ["replaceText", "none", 107, "beforeInput", "add"],
+            ["replaceText", "none", 109, "beforeInput", "subtract"],
+            ["replaceText", "none", 110, "beforeInput", "decimal point"],
+            ["replaceText", "none", 111, "beforeInput", "divide"],
+            ["replaceText", "none", 186, "beforeInput", ";"],
+            ["replaceText", "none", 187, "beforeInput", "="],
+            ["replaceText", "none", 188, "beforeInput", ","],
+            ["replaceText", "none", 189, "beforeInput", "-"],
+            ["replaceText", "none", 190, "beforeInput", "."],
+            ["replaceText", "none", 219, "beforeInput", "["],
+            ["replaceText", "none", 220, "beforeInput", "\\"],
+            ["replaceText", "none", 221, "beforeInput", "]"],
+            ["replaceText", "none", 222, "beforeInput", "'"],
+            ["modify", "shift", 37, "beforeSelectionChange", ""],
+            ["modify", "shift", 38, "beforeSelectionChange", ""],
+            ["modify", "shift", 39, "beforeSelectionChange", ""],
+            ["modify", "shift", 40, "beforeSelectionChange", ""],
+            ["replaceText", "shift", 65, "beforeInput", "A"],
+            ["replaceText", "shift", 66, "beforeInput", "B"],
+            ["replaceText", "shift", 67, "beforeInput", "C"],
+            ["replaceText", "shift", 68, "beforeInput", "D"],
+            ["replaceText", "shift", 69, "beforeInput", "E"],
+            ["replaceText", "shift", 70, "beforeInput", "F"],
+            ["replaceText", "shift", 71, "beforeInput", "G"],
+            ["replaceText", "shift", 72, "beforeInput", "H"],
+            ["replaceText", "shift", 73, "beforeInput", "I"],
+            ["replaceText", "shift", 74, "beforeInput", "J"],
+            ["replaceText", "shift", 75, "beforeInput", "K"],
+            ["replaceText", "shift", 76, "beforeInput", "L"],
+            ["replaceText", "shift", 77, "beforeInput", "M"],
+            ["replaceText", "shift", 78, "beforeInput", "N"],
+            ["replaceText", "shift", 79, "beforeInput", "O"],
+            ["replaceText", "shift", 80, "beforeInput", "P"],
+            ["replaceText", "shift", 81, "beforeInput", "Q"],
+            ["replaceText", "shift", 82, "beforeInput", "R"],
+            ["replaceText", "shift", 83, "beforeInput", "S"],
+            ["replaceText", "shift", 84, "beforeInput", "T"],
+            ["replaceText", "shift", 85, "beforeInput", "U"],
+            ["replaceText", "shift", 86, "beforeInput", "V"],
+            ["replaceText", "shift", 87, "beforeInput", "W"],
+            ["replaceText", "shift", 88, "beforeInput", "X"],
+            ["replaceText", "shift", 89, "beforeInput", "Y"],
+            ["replaceText", "shift", 90, "beforeInput", "Z"],
+            ["selectall", "control", 65, "beforeSelectionChange", "NULL"], 
+            ["formatContent", "control", 66, "beforeInput", "bold:toggle"], 
+            ["copy", "control", 67, "clipboard", "N/A"], 
+            ["formatContent", "control", 73, "beforeInput", "italic:toggle"], 
+            ["formatContent", "control", 85, "beforeInput", "underline:toggle"], 
+            ["paste", "control", 86, "clipboard", "N/A"], 
+            ["cut", "control", 88, "clipboard", "N/A"],
+            ["redo", "control", 89, "beforeInput", "NULL"], 
+            ["undo", "control", 90, "beforeInput", "NULL"]];    
             
         supportedIntentions.getIntentionDetails = 
             function(name)
